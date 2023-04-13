fb7e45ec8d9fc8dd029d2a5ab5589c0fb4937a71
asurelaba <asuresh.laba@solvd.com>
2023-04-13 06:37:03 -0400

    Adding Hello User program file

---
+++ HelloUser.java
@@ -0,0 +1,26 @@
+/*
+ * Ashwini Suresh
+ * Copyright (c) Ashwini Suresh All Rights Reserved.
+ * */
+
+/*
+ * Class HelloUser prints the user name that is passed to main method through command line.
+ *
+ * @version 1.0 12 Apr 2023
+ * @author Ashwini Suresh
+ * */
+public class HelloUser {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.print("Hello ");
+            for (String name : args) {
+                System.out.print(name + ' ');
+            }
+
+        } else {
+            System.out.println("Hello user");
+        }
+
+    }
+}
